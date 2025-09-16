93fc66167075bfe39e6182657e64719b3746c890
Fmar <frnandu@gmail.com>
2025-09-17 00:36:28 +0200

    add apk png

--- lib/i18n/gen/strings.g.dart
+++ lib/i18n/gen/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 557 (278 per locale)
 ///
-/// Built on 2025-09-16 at 22:19 UTC
+/// Built on 2025-09-16 at 22:33 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
