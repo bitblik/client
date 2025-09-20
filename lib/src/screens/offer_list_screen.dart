db354bb26b1d6d367c62714069ea0a4945363f26
Fmar <frnandu@gmail.com>
2025-09-20 02:39:22 +0200

    change simplex/matrix group links to test

--- lib/src/screens/offer_list_screen.dart
+++ lib/src/screens/offer_list_screen.dart
@@ -483,7 +483,8 @@ class _OfferListScreenState extends ConsumerState<OfferListScreen> {
                 child: InkWell(
                   onTap: () async {
                     final Uri url = Uri.parse(
-                      'https://simplex.chat/contact#/?v=2-7&smp=smp%3A%2F%2Fu2dS9sG8nMNURyZwqASV4yROM28Er0luVTx5X1CsMrU%3D%40smp4.simplex.im%2FjwS8YtivATVUtHogkN2QdhVkw2H6XmfX%23%2F%3Fv%3D1-3%26dh%3DMCowBQYDK2VuAyEAsNpGcPiALZKbKfIXTQdJAuFxOmvsuuxMLR9rwMIBUWY%253D%26srv%3Do5vmywmrnaxalvz6wi3zicyftgio6psuvyniis6gco6bp6ekl4cqj4id.onion&data=%7B%22groupLinkId%22%3A%22hCkt5Ph057tSeJdyEI0uug%3D%3D%22%7D',
+                      //'https://simplex.chat/contact#/?v=2-7&smp=smp%3A%2F%2Fu2dS9sG8nMNURyZwqASV4yROM28Er0luVTx5X1CsMrU%3D%40smp4.simplex.im%2FjwS8YtivATVUtHogkN2QdhVkw2H6XmfX%23%2F%3Fv%3D1-3%26dh%3DMCowBQYDK2VuAyEAsNpGcPiALZKbKfIXTQdJAuFxOmvsuuxMLR9rwMIBUWY%253D%26srv%3Do5vmywmrnaxalvz6wi3zicyftgio6psuvyniis6gco6bp6ekl4cqj4id.onion&data=%7B%22groupLinkId%22%3A%22hCkt5Ph057tSeJdyEI0uug%3D%3D%22%7D',
+                      'https://simplex.chat/contact#/?v=2-7&smp=smp%3A%2F%2Fu2dS9sG8nMNURyZwqASV4yROM28Er0luVTx5X1CsMrU%3D%40smp4.simplex.im%2F-FjYjoPVW323UWnxJ-ICEIvlUY0vnuRM%23%2F%3Fv%3D1-4%26dh%3DMCowBQYDK2VuAyEAX-eUfNzP4E_n0BkC-5A7iqHrchhcDC23FopK4JPXm3Q%253D%26q%3Dc%26srv%3Do5vmywmrnaxalvz6wi3zicyftgio6psuvyniis6gco6bp6ekl4cqj4id.onion&data=%7B%22groupLinkId%22%3A%22pG-_A9dIAhbdz8ZTTpbNdQ%3D%3D%22%7D'
                     );
                     await launchUrl(url);
                   },
@@ -512,7 +513,8 @@ class _OfferListScreenState extends ConsumerState<OfferListScreen> {
                 child: InkWell(
                   onTap: () async {
                     final Uri url = Uri.parse(
-                      'https://matrix.to/#/#bitblik-offers:matrix.org',
+                      // 'https://matrix.to/#/#bitblik-offers:matrix.org',
+                      'https://matrix.to/#/#test-bitblik-offers:matrix.org'
                     );
                     await launchUrl(url);
                   },
